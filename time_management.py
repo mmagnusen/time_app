b54a7aed496bc3409779cd86c0b28765f597ee45
marilyn52 <m-mags@hotmail.co.uk>
2017-08-09 13:58:53 +0100

    first version

---
+++ time_management.py
@@ -0,0 +1,7 @@
+# Welcome message, something like "Hi Marilyn! Well done on keeping up-to-date with your schedule. Let's help keep you on track :)"
+
+# If I haven't entered one of these values for this particular day, there will be a prompt to ask me: how long at gym, how long at work, how long coding, how long socialising
+
+# I should be able to enter a command which tells the app I've finished entering my hours for today
+
+# It should give me a summary of each thing I've done today
